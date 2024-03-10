f38f06f7508f0795b6cfb4ff516d670963d24978
ghoulslash <41651341+ghoulslash@users.noreply.github.com>
2024-03-10 17:49:00 -0400

    fix repeated quick claw/quick draw checks (#4266)
    * fix repeated quick claw/quick draw checks
    
    * fix field names
    
    ---------
    
    Co-authored-by: ghoulslash <pokevoyager0@gmail.com>

--- include/battle.h
+++ include/battle.h
@@ -744,6 +744,8 @@ struct BattleStruct
     u8 intrepidSwordBoost[NUM_BATTLE_SIDES];
     u8 dauntlessShieldBoost[NUM_BATTLE_SIDES];
     u8 supersweetSyrup[NUM_BATTLE_SIDES];
+    u8 quickClawRandom[MAX_BATTLERS_COUNT];
+    u8 quickDrawRandom[MAX_BATTLERS_COUNT];
     u8 supremeOverlordCounter[MAX_BATTLERS_COUNT];
     u8 boosterEnergyActivates;
 };
