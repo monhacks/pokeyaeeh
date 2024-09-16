191e443ecbfc7648a2f2ac2f387f4fe51ee14f3d
PCG06 <pcggaming06@gmail.com>
2024-09-16 14:41:00 +0530

    Fix oopsie

--- src/data/graphics/trainers.h
+++ src/data/graphics/trainers.h
@@ -295,14 +295,14 @@ const u8 gTrainerBackPic_Steven[] = INCBIN_U8("graphics/trainers/back_pics/steve
 const u32 gTrainerBackPicPalette_Red[] = INCBIN_U32("graphics/trainers/back_pics/red.gbapal.lz");
 const u32 gTrainerBackPicPalette_Leaf[] = INCBIN_U32("graphics/trainers/back_pics/leaf.gbapal.lz");
 
-const u32 gTrainerFrontPic_AsianDad[] = INCBIN_U32("graphics/trainers/front_pics/custom/asian_dad.4bpp.lz");
-const u32 gTrainerPalette_AsianDad[] = INCBIN_U32("graphics/trainers/front_pics/custom/asian_dad.gbapal.lz");
+const u32 gTrainerFrontPic_AsianDad[] = INCBIN_U32("graphics/trainers/front_pics/asian_dad.4bpp.lz");
+const u32 gTrainerPalette_AsianDad[] = INCBIN_U32("graphics/trainers/front_pics/asian_dad.gbapal.lz");
 
-const u32 gTrainerFrontPic_Cynthia[] = INCBIN_U32("graphics/trainers/front_pics/custom/cynthia.4bpp.lz");
-const u32 gTrainerPalette_Cynthia[] = INCBIN_U32("graphics/trainers/front_pics/custom/cynthia.gbapal.lz");
+const u32 gTrainerFrontPic_Cynthia[] = INCBIN_U32("graphics/trainers/front_pics/cynthia.4bpp.lz");
+const u32 gTrainerPalette_Cynthia[] = INCBIN_U32("graphics/trainers/front_pics/cynthia.gbapal.lz");
 
-const u32 gTrainerFrontPic_Lance[] = INCBIN_U32("graphics/trainers/front_pics/custom/lance.4bpp.lz");
-const u32 gTrainerPalette_Lance[] = INCBIN_U32("graphics/trainers/front_pics/custom/lance.gbapal.lz");
+const u32 gTrainerFrontPic_Lance[] = INCBIN_U32("graphics/trainers/front_pics/lance.4bpp.lz");
+const u32 gTrainerPalette_Lance[] = INCBIN_U32("graphics/trainers/front_pics/lance.gbapal.lz");
 
 // The first two parameters invoke a front pic and palette by
 // calling a "TRAINER_PIC" constant (e.g. TRAINER_PIC_HIKER), and
